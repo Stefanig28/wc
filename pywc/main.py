f72ad68260f6164aa5ba718618b114a3cc34c2a5
Stefani <stefanigtorres@gmail.com>
2024-10-17 01:33:03 -0500

    optimizacion protecto

--- pywc/main.py
+++ pywc/main.py
@@ -3,52 +3,57 @@ import pathlib
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filepath", help="archivo a procesar", type=pathlib.Path)
+
 parser.add_argument("-c", "--count-bytes", help="contador de bytes",
                     action="store_true")
+
 parser.add_argument("-l", "--count-lines", help="contador de lineas",
                     action="store_true")
+
 parser.add_argument("-w", "--count-words", help="contador de palabras",
                     action="store_true")
+
 parser.add_argument("-m", "--count-characters", help="contador de caracteres",
                     action="store_true")
 
 args = parser.parse_args()
 
-def count_bytes(filepath):
-    with open(args.filepath, "r", encoding='utf-8') as reader:
-        return len(reader.read())
-    
-def count_lines(filepath):
-    with open(args.filepath, "r", encoding='utf-8') as reader:
-        return len(reader.readlines())
-    
-def count_words(filepath):
-    with open(args.filepath, "r", encoding='utf-8') as reader:
-        return len(reader.read().split())
-    
-def count_characters(filepath):
-    with open(args.filepath, "r", encoding='utf-8') as reader:
-        return sum(len(line) for line in reader.read().splitlines())
-    
-def structure() -> str:
+def calculete_result():
     result = []
 
     if args.count_bytes:
-        result.append(count_bytes(args.filepath))
+        with open(args.filepath, "rb") as reader:
+            bytes_content = reader.read()
+            result.append(len(bytes_content))
+
+    with open(args.filepath, "r", encoding='utf-8') as reader:
+        content = reader.read()
 
     if args.count_lines:
-        result.append(count_lines(args.filepath))
+        calculate_lines = content.count('\n') + 1
+        result.append(calculate_lines)
 
     if args.count_words:
-        result.append(count_words(args.filepath))
+        calculate_words = len(content.split())
+        result.append(calculate_words)
 
     if args.count_characters:
-        result.append(count_characters(args.filepath))
+        calculate_characters = len(content)
+        result.append(calculate_characters)
 
-    if len(result) < 1:
-        return f"{count_bytes(args.filepath)} {count_lines(args.filepath)} {count_words(args.filepath)} {args.filepath}"
-    else:
-        return " ".join(str(num) for num in result) + " " + str(args.filepath)
+    return result
+
+    
+def structure() -> str:
+
+    if not (args.count_bytes or args.count_lines or args.count_words or args.count_characters):
+        args.count_lines = True
+        args.count_words = True
+        args.count_characters = True
+    
+    string = " ".join(str(num) for num in calculete_result()) + " " + str(args.filepath)
+
+    return string
 
 print(structure())
 
